72e5f3cc4bfc91ea8c19dccaedd34cb7ca81d6f8
jbkad <jkadibu@ic.ac.uk>
2022-08-11 04:10:20 +0100

    committing

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {sequelize} = require('./db')
+const {db} = require('./db')
 const {Restaurant, Menu} = require('./models/index')
 const {
     seedRestaurant,
@@ -13,31 +13,41 @@ describe('Restaurant and Menu Models', () => {
         // the 'sync' method will create tables based on the model class
         // by setting 'force:true' the tables are recreated each time the 
         // test suite is run
-        await sequelize.sync({ force: true });
+        await db.sync({ force: true });
     });
 
+    
+        // DONE - write test(1)
     test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+    const testRestaurant = await Restaurant.create(seedRestaurant[0])
+    expect(testRestaurant.name).toEqual(seedRestaurant[0].name)
     });
 
+
+        // DONE - write test(2)
     test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const testMenu = await Menu.create(seedMenu[0])
+        expect(testMenu.name).toEqual(seedMenu[0].name)
     });
 
+
+        // TODO - write test(3)
     test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const test_ = await _.create(seed_[0])
+        expect(test_.name).toEqual(seed_[0].name)
     });
 
+
+        // TODO - write test(3)
     test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const test_ = await _.create(seed_[0])
+        expect(test_.name).toEqual(seed_[0].name)
     });
 
+
+        // TODO - write test(4)
     test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const test_ = await _.create(seed_[0])
+        expect(test_.name).toEqual(seed_[0].name)
     });
 })
--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,19 @@
-const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const {db} = require('../db');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// TODO - create a Menu model
+// DONE - create a Menu model
+const Menu = db.define('menu',{
+    title: {
+        type: DataTypes.STRING
+    }
+})
+
+async function main(){
+    await Menu.sync()
+
+    await Menu.create({
+        title: 'saver menu'
+    })
+}
 
 module.exports = {Menu};
